05fe7875c71e320c66c1e1d732f33a68a2098310
ChrisSalinasSoto <SSSillyMac@MacBook-Pro-van-Christofer.local>
2016-09-19 12:40:15 +0200

    .

---
+++ 1a.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+$nameErr = "";
+?>
+<form method="post" action="1a_process.php">
+    Naam:<br><input type="text" name="name">
+    <br>
+    Aantal fouten:<br><input type="number" name="number"><br>
+    <input type="submit" value="verwerk">
+</form>
+</body>
+</html>
+
---
+++ 1a_process.php
@@ -0,0 +1,20 @@
+<?php
+//print_r($_POST);
+
+if($_POST['name'] == ""){
+    echo "je moet wel je naam in komen te vullen <br>";
+}else{
+    echo "naam ".$_POST["name"]."<br>";
+}
+
+if($_POST['number'] == ""){
+    echo "je moet wel aantal fouten in te vullen <br>";
+}elseif($_POST['number'] >= 100){
+    echo "je kan niet meer dan 99 fouten hebben";
+}else{
+echo "aantal fouten ".$_POST["number"]."<br>";
+}
+
+echo "<a href='1a.php'>Terug naar formulier</a>";
+?>
+
